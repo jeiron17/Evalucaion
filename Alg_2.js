3a60ca135ff39e19db21a4b4fae77e207323531b
jeiron17 <113035739+jeiron17@users.noreply.github.com>
2023-09-07 08:40:30 -0500

    Add files via upload

---
+++ Alg_2.js
@@ -0,0 +1,8 @@
+var sueldo = Number(1300000)
+var bono = Number(100000)
+var promedio_año = (sueldo*12)
+var bo = bono*12
+var resul = promedio_año+bo
+
+alert("su sueldo anual es de:"+resul)
+console.log("su sueldo anual es de:",resul)
